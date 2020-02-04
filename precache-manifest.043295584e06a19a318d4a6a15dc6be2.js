cb94b6d36c69a9cdf63004d554d215600c0293ad
Dominic Chu <dominic.m.chu@gmail.com>
2020-02-04 12:20:50 -0500

    Updates

---
+++ precache-manifest.043295584e06a19a318d4a6a15dc6be2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc9cc4680b3c1d305ce02505b1cbe100",
+    "url": "/atanon/index.html"
+  },
+  {
+    "revision": "8bfcca6fbdd6b5a5cba5",
+    "url": "/atanon/static/css/main.912e9e72.chunk.css"
+  },
+  {
+    "revision": "8b524f4349cca3df2bcc",
+    "url": "/atanon/static/js/2.50a7bdad.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/atanon/static/js/2.50a7bdad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8bfcca6fbdd6b5a5cba5",
+    "url": "/atanon/static/js/main.59f21c60.chunk.js"
+  },
+  {
+    "revision": "e272df7c0505724cb01c",
+    "url": "/atanon/static/js/runtime-main.ab3038ef.js"
+  }
+]);
